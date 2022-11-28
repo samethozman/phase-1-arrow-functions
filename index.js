1e35a56146974c6fd7d33242853d36693154c110
samethozman <samet.hozman@outlook.com>
2022-11-28 13:33:59 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (param1, param2) => 2000 / 100;
+
+const square = (i) => i ** i; // VS Code somehow adds the parentheses around 'i' by default whenever I save the file, even though it's only a single parameter
+
+const add = (x, y) => x + y;
